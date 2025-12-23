8493b00fef3242753a9c7530a56a7508ee1bd2f9
bala-amypo <bala@amypo.in>
2025-12-23 04:45:41 +0000

    User commit

--- src/main/java/com/example/demo/controller/AuthController.java
+++ src/main/java/com/example/demo/controller/AuthController.java
@@ -1,18 +1,34 @@
 package com.example.demo.controller;
 
+import com.example.demo.dto.AuthRequestDto;
+import com.example.demo.dto.AuthResponseDto;
+import com.example.demo.dto.RegisterRequestDto;
+import com.example.demo.service.AuthService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/auth")
 public class AuthController {
 
+    private final AuthService authService;
+
+    public AuthController(AuthService authService) {
+        this.authService = authService;
+    }
+
     @PostMapping("/register")
-    public String register() {
-        return "User registered";
+    public ResponseEntity<String> register(
+            @RequestBody RegisterRequestDto request) {
+
+        authService.register(request);
+        return ResponseEntity.ok("User registered");
     }
 
     @PostMapping("/login")
-    public String login() {
-        return "JWT_TOKEN";
+    public ResponseEntity<AuthResponseDto> login(
+            @RequestBody AuthRequestDto request) {
+
+        return ResponseEntity.ok(authService.login(request));
     }
 }
