64b1ef64bfbfd4992f0001f48208e7d65630f1d6
bala-amypo <bala@amypo.in>
2025-12-26 03:56:41 +0000

    User commit

--- src/main/java/com/example/demo/dto/AuthRequest.java
+++
@@ -1,7 +0,0 @@
-package com.example.demo.dto;
-
-/**
- * Alias class required ONLY for tests
- */
-public class AuthRequest extends AuthRequestDto {
-}
--- src/main/java/com/example/demo/repository/BreachRuleRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.demo.repository;
-
-import com.example.demo.entity.BreachRule;
-import java.util.Optional;
-
-public interface BreachRuleRepository {
-    Optional<BreachRule> findFirstByActiveTrueOrderByDefaultRuleDesc();
-}
